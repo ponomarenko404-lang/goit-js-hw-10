0e1998b21f9b015aa4d42df54c4b2c4a45bc6a5a
ponomarenko404-lang <ponomarenko404@gmail.com>
2026-03-07 11:45:21 +0000

    Deploying to gh-pages from @ ponomarenko404-lang/goit-js-hw-10@a692544bf85366a6b3aa5db21b167a088fedec7c 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as r}from"./assets/vendor-BbbuE1sJ.js";const i=document.querySelector(".form");i.addEventListener("submit",t=>{t.preventDefault();const s=Number(t.currentTarget.elements.delay.value),m=t.currentTarget.elements.state.value;new Promise((e,o)=>{setTimeout(()=>{m==="fulfilled"?e(s):o(s)},s)}).then(e=>{r.success({message:`Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({message:`Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
